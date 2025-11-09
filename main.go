a9f9b1984344887569b94f9641da72f4d2efbfa0
JasmineAlves <jascpz.tec@gmail.com>
2025-11-09 15:19:27 -0300

    feat: add router subpackage

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/JasmineAlves/gopportunities-project.git/router"
+
+// OBS: No go, imports e exports são por convenção de nomenclatura (R = exportada, r = não exportada/var local)
+
+func main() {
+
+	// Inicializar router
+	router.Initialize()
+}
